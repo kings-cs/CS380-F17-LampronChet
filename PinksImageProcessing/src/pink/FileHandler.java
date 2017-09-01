761d0125a0f78545439363f8b15eab79605561d0
cjlampron13 <cjlampron13@gmail.com>
2017-09-01 19:36:56 -0400

    maximize screen when picture is loaded

--- PinksImageProcessing/src/pink/FileHandler.java
+++ PinksImageProcessing/src/pink/FileHandler.java
@@ -1,5 +1,19 @@
 package pink;
 
-public class FileHandler {
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+
+public class FileHandler {
+	
+	public BufferedImage createImage(String filePath) throws IOException {
+		File file = new File(filePath);
+		BufferedImage readIn = ImageIO.read(file);
+		
+		BufferedImage returnImage = new BufferedImage(readIn.getWidth(), readIn.getHeight(), BufferedImage.TYPE_INT_ARGB);
+		return returnImage;
+	}
 }
--- PinksImageProcessing/src/pink/PipGui.java
+++ PinksImageProcessing/src/pink/PipGui.java
@@ -3,13 +3,14 @@
  */
 package pink;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFileChooser;
@@ -18,6 +19,7 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 
@@ -33,8 +35,16 @@ public class PipGui extends JFrame {
 	 * Default generated ID.
 	 */
 	private static final long serialVersionUID = 1L;
+	/** The instance of the FileHandler class. */
+	private FileHandler fileHandler;
+	/** The main back panel. */
+	private JPanel backPanel;
+	/** The backPanels center component. */
+	private JPanel centerPanel;
 
 	public PipGui() {
+		fileHandler = new FileHandler();
+		
 		this.setTitle("Pinks Image Processor");
 		String iconPath = "Docs/PinkIcon.jpg";
 		ImageIcon icon = new ImageIcon(iconPath);
@@ -43,12 +53,24 @@ public class PipGui extends JFrame {
 		JMenuBar menuBar = new JMenuBar();
 		this.setJMenuBar(menuBar);
 		JMenu file = new JMenu("File");
+		JMenu options = new JMenu("Options");
+		JMenuItem grayscale = new JMenuItem("Grayscale");
+		options.add(grayscale);
 		JMenu about = new JMenu("About");
 		menuBar.add(file);
+		menuBar.add(options);
 		menuBar.add(about);
 		JMenuItem open = new JMenuItem("Open");
 		open.addActionListener(new OpenFile());
 		file.add(open);
+		
+		backPanel = new JPanel();
+		backPanel.setLayout(new BorderLayout());
+		this.add(backPanel);
+		centerPanel = new JPanel();
+		centerPanel.setLayout(new FlowLayout());
+		backPanel.add(centerPanel, BorderLayout.CENTER);
+		
 
 	}
 
@@ -61,7 +83,18 @@ public class PipGui extends JFrame {
 
 			if (val == JFileChooser.APPROVE_OPTION) {
 				String filePath = choose.getSelectedFile().getAbsolutePath();
-				JOptionPane.showConfirmDialog(null, filePath);
+				BufferedImage image = null;
+				try {
+					image = fileHandler.createImage(filePath);
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, "Image could not be processed");
+				}
+				Graphics g = image.getGraphics();
+				centerPanel.paintComponents(g);
+				g.drawImage(image, 15, 15, centerPanel);
+				getGui().setExtendedState(getGui().getExtendedState() | JFrame.MAXIMIZED_BOTH);
+				//centerPanel.repaint();
+				//JOptionPane.showConfirmDialog(null, filePath);
 			}
 		}
 
@@ -82,5 +115,9 @@ public class PipGui extends JFrame {
 		PipGui display = new PipGui();
 		display.setSize(400, 400);
 		display.setVisible(true);
+	}
+	
+	private PipGui getGui() {
+		return this;
 	}
 }
