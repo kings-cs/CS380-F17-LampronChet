be644f1bbebcdadc72d4d2d828a966316c0069f6
cjlampron13 <cjlampron13@gmail.com>
2017-09-02 13:06:25 -0400

    Attempted to add sides

--- PinksImageProcessing/src/pink/PipGui.java
+++ PinksImageProcessing/src/pink/PipGui.java
@@ -28,7 +28,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 /**
  * GUI for an image processor.
  * 
- * @author Chet
+ * @author Chet Lampron
  *
  */
 public class PipGui extends JFrame {
@@ -45,8 +45,18 @@ public class PipGui extends JFrame {
 	private CenterPainter centerPanel;
 	/** The current image. */
 	private BufferedImage image;
+	/** The current Gui. */
+	private PipGui currentGui;
+	/** The temporary side walls. */
+	private BufferedImage sideMural;
+	/** The side panels. */
+	private MuralPanel sides;
 
-	public PipGui() {
+	/**
+	 * Constructor for a PipGui.
+	 * @throws IOException 
+	 */
+	public PipGui() throws IOException {
 		fileHandler = new FileHandler();
 
 		this.setTitle("Pinks Image Processor");
@@ -74,15 +84,32 @@ public class PipGui extends JFrame {
 		centerPanel = new CenterPainter();
 		centerPanel.setLayout(new FlowLayout());
 		backPanel.add(centerPanel, BorderLayout.CENTER);
+		
+		
+		sides = new MuralPanel();
+		backPanel.add(sides, BorderLayout.EAST);
+		backPanel.add(sides, BorderLayout.WEST);
+		String borderFilePath = "Docs/sideMural.jpg";
+		BufferedImage borderImage = fileHandler.createImage(borderFilePath);
+		Graphics borderGraphics = borderImage.getGraphics();
+		//borderGraphics.drawImage(borderImage, 0, 0, sides);
+		sides.paintComponent(borderGraphics);
+		
 
 	}
 
+	/**
+	 * Class to listen to the open option button.
+	 * 
+	 * @author Chet
+	 *
+	 */
 	private class OpenFile implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
 			JFileChooser choose = new JFileChooser("Select your image: ");
-			FileFilter filter = new FileNameExtensionFilter("Pictures", new String[] {"jpg", "jpeg", "png", "gif"});
+			FileFilter filter = new FileNameExtensionFilter("Pictures", new String[] { "jpg", "jpeg", "png", "gif" });
 			choose.setFileFilter(filter);
 			int val = choose.showOpenDialog(null);
 
@@ -93,9 +120,11 @@ public class PipGui extends JFrame {
 				try {
 					image = fileHandler.createImage(filePath);
 					centerPanel.setSize(image.getWidth(), image.getHeight());
+
 					g = image.getGraphics();
 					// getGui().setExtendedState(getGui().getExtendedState() |
 					// JFrame.MAXIMIZED_BOTH);
+					g.drawImage(image, 0, 0, centerPanel);
 					centerPanel.paintComponent(g);
 				} catch (IOException e) {
 					JOptionPane.showMessageDialog(null, "Image could not be processed");
@@ -120,13 +149,44 @@ public class PipGui extends JFrame {
 			JOptionPane.showMessageDialog(null, "The Nimbus look and feel is not available");
 		}
 
-		PipGui display = new PipGui();
+		PipGui display = null;
+		try {
+			display = new PipGui();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, "Borders could not be found");
+			System.exit(ERROR);
+		}
+		display.setCurrentGui(display);
 		display.setSize(400, 400);
 		display.setVisible(true);
 	}
 
+	/**
+	 * In class use only.
+	 * 
+	 * @return The current Gui.
+	 */
 	private PipGui getGui() {
 		return this;
+	}
+	
+	private void setCurrentGui(PipGui current) {
+		currentGui = current;
+	}
+	/**
+	 * 
+	 * @author Chet
+	 *
+	 */
+	private class MuralPanel extends JPanel {
+
+		@Override
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			if(sideMural != null) {
+				g.fillRect(0, 0, sides.getWidth(), sides.getHeight());
+			}
+		}
 	}
 
 	private class CenterPainter extends JPanel {
