7f593226b4ed9007051b84c585eaab81de1e94f3
cjlampron13 <cjlampron13@gmail.com>
2017-09-19 19:15:04 -0400

    For question to Dr. Jump

--- PinksImageProcessing/src/algorithms/BlurModifier.java
+++ PinksImageProcessing/src/algorithms/BlurModifier.java
@@ -12,36 +12,28 @@ import java.awt.image.Raster;
 public class BlurModifier extends PixelModifier {
 	@Override
 	public BufferedImage modifyPixel(BufferedImage image) {
-		//long startTime = System.nanoTime();
+		// long startTime = System.nanoTime();
 		int width = image.getWidth();
 		int height = image.getHeight();
 
 		int[] sourceData = super.unwrapImage(image);
-		
+
 		int[] redArray = new int[sourceData.length];
 		int[] blueArray = new int[sourceData.length];
 		int[] greenArray = new int[sourceData.length];
 		int[] alphaArray = new int[sourceData.length];
-		
-		int rCount = 0;
-		int bCount = 0;
-		int gCount = 0;
-		int aCount = 0;
+
 		System.out.println(sourceData[0]);
-		for(int i = 0; i < sourceData.length; i++) {
-			if(sourceData[i] == 'R') {
-				redArray[aCount] = sourceData[i];
-				rCount++;
-			}else if(sourceData[i] == 'B') {
-				blueArray[aCount] = sourceData[i];
-				bCount++;
-			}else if(sourceData[i] == 'G') {
-				greenArray[aCount] = sourceData[i];
-				gCount++;
-			}else if(sourceData[i] == 'A' ) {
-				alphaArray[aCount] = sourceData[i];
-				aCount++;
-			}
+		for (int i = 0; i < sourceData.length; i++) {
+			int alpha = (sourceData[i] & PixelModifier.ALPHA_MASK) >> PixelModifier.ALPHA_OFFSET;
+			int red = (sourceData[i] & PixelModifier.RED_MASK) >> PixelModifier.RED_OFFSET;
+			int green = (sourceData[i] & PixelModifier.GREEN_MASK) >> PixelModifier.GREEN_OFFSET;
+			int blue = (sourceData[i] & PixelModifier.BLUE_MASK) >> PixelModifier.BLUE_OFFSET;
+			
+			redArray[i] = red;
+			blueArray[i] = blue;
+			greenArray[i] = green;
+			alphaArray[i] = alpha;
 		}
 
 		int[] resultData = new int[sourceData.length];
@@ -55,11 +47,9 @@ public class BlurModifier extends PixelModifier {
 				int red = (pixel & PixelModifier.RED_MASK) >> PixelModifier.RED_OFFSET;
 				int green = (pixel & PixelModifier.GREEN_MASK) >> PixelModifier.GREEN_OFFSET;
 				int blue = (pixel & PixelModifier.BLUE_MASK) >> PixelModifier.BLUE_OFFSET;
-
 				
 				
-				
-				
+				//TODO blur
 				
 				int newPixel = (alpha << ALPHA_OFFSET) | (red << RED_OFFSET) | (blue << BLUE_OFFSET)
 						| (green << GREEN_OFFSET);
@@ -73,7 +63,8 @@ public class BlurModifier extends PixelModifier {
 		Raster resultRastor = Raster.createRaster(image.getRaster().getSampleModel(), resultDataBuffer,
 				new Point(0, 0));
 		image.setData(resultRastor);
-		//JOptionPane.showMessageDialog(null, "Total Time: " + (System.nanoTime() - startTime) / 1000000 + "ms");
+		// JOptionPane.showMessageDialog(null, "Total Time: " + (System.nanoTime() -
+		// startTime) / 1000000 + "ms");
 		return image;
 	}
 }
