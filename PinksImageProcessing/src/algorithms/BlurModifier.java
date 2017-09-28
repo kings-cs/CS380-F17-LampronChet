24b6a7e998f35c6b3b88cd7894c58ced33148294
cjlampron13 <cjlampron13@gmail.com>
2017-09-28 00:45:11 -0400

    Blur works now
    changed chain of if elses to new logic and removed slow downs in loop

--- PinksImageProcessing/src/algorithms/BlurModifier.java
+++ PinksImageProcessing/src/algorithms/BlurModifier.java
@@ -30,60 +30,156 @@ public class BlurModifier extends PixelModifier {
 		int[] blueArray = new int[sourceData.length];
 		int[] greenArray = new int[sourceData.length];
 		int[] alphaArray = new int[sourceData.length];
-		int[] modifiedRedArray = null;
-		int[] modifiedGreenArray = null;
-		int[] modifiedBlueArray = null;
-		
-		for (int i = 0; i < sourceData.length; i++) {
-			int alpha = (sourceData[i] & PixelModifier.ALPHA_MASK) >> PixelModifier.ALPHA_OFFSET;
-			int red = (sourceData[i] & PixelModifier.RED_MASK) >> PixelModifier.RED_OFFSET;
-			int green = (sourceData[i] & PixelModifier.GREEN_MASK) >> PixelModifier.GREEN_OFFSET;
-			int blue = (sourceData[i] & PixelModifier.BLUE_MASK) >> PixelModifier.BLUE_OFFSET;
+		int[] modifiedRedArray = new int[redArray.length];
+		int[] modifiedGreenArray = new int[greenArray.length];
+		int[] modifiedBlueArray = new int[blueArray.length];
 
-			redArray[i] = red;
-			blueArray[i] = blue;
-			greenArray[i] = green;
-			alphaArray[i] = alpha;
+		for (int row = 0; row < height; row++) {
+			for (int col = 0; col < width; col++) {
+				int index = row * width + col;
+				int alpha = (sourceData[index] & PixelModifier.ALPHA_MASK) >> PixelModifier.ALPHA_OFFSET;
+				int red = (sourceData[index] & PixelModifier.RED_MASK) >> PixelModifier.RED_OFFSET;
+				int green = (sourceData[index] & PixelModifier.GREEN_MASK) >> PixelModifier.GREEN_OFFSET;
+				int blue = (sourceData[index] & PixelModifier.BLUE_MASK) >> PixelModifier.BLUE_OFFSET;
+
+				redArray[index] = red;
+				blueArray[index] = blue;
+				greenArray[index] = green;
+				alphaArray[index] = alpha;
+			}
 		}
-		System.out.println("here0");
 		int[] resultData = new int[sourceData.length];
 
 		for (int row = 0; row < height; row++) {
 			for (int col = 0; col < width; col++) {
 				int index = row * width + col;
 
-				modifiedRedArray = new int[redArray.length];
-				modifiedGreenArray = new int[greenArray.length];
-				modifiedBlueArray = new int[blueArray.length];
-				
 				int count = 0;
 				int redModify = 0;
 				int greenModify = 0;
 				int blueModify = 0;
-				System.out.println("before inner fors");
-				for(int stenRow = row - 2; stenRow <= row + 2; stenRow++) {
-					for(int stenCol = col - 2; stenCol <= col + 2; stenCol++) {
-						if(stenRow >= 0 && stenRow <= height && stenCol >= 0 && stenCol <= width) {
+				for (int stenRow = row - 2; stenRow <= row + 2; stenRow++) {
+					for (int stenCol = col - 2; stenCol <= col + 2; stenCol++) {
+					/*	
+						if (stenRow >= 0 && stenRow < height && stenCol >= 0 && stenCol < width) {
 							int anIndex = stenRow * width + stenCol;
 							redModify += (int) redArray[anIndex] * stencil[count];
 							greenModify += (int) greenArray[anIndex] * stencil[count];
 							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow < 0 && stenCol < 0) {
+							int newRow = 0;
+							int newCol = 0;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow < 0 && stenCol >= 0) {
+							int newRow = 0;
+							int newCol = stenCol;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow >= 0 && stenCol < 0) {
+							int newRow = stenRow;
+							int newCol = 0;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow < 0 && stenCol >= width) {
+							int newRow = 0;
+							int newCol = width - 1;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow >= 0 && stenCol >= width) {
+							int newRow = stenRow;
+							int newCol = width - 1;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow < 0 && stenCol < width) {
+							int newRow = 0;
+							int newCol = stenCol;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow >= height && stenCol >= width) {
+							int newRow = height - 1;
+							int newCol = width - 1;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow < height && stenCol >= width) {
+							int newRow = stenRow;
+							int newCol = width - 1;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow >= height && stenCol < width) {
+							int newRow = height - 1;
+							int newCol = stenCol;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						} else if (stenRow >= height && stenCol < 0) {
+							int newRow = height - 1;
+							int newCol = 0;
+							int anIndex = newRow * width + newCol;
+							redModify += (int) redArray[anIndex] * stencil[count];
+							greenModify += (int) greenArray[anIndex] * stencil[count];
+							blueModify += (int) blueArray[anIndex] * stencil[count];
+							count++;
+						}*/
+						
+						int newRow = stenRow;
+						int newCol = stenCol;
+						
+						if(newRow < 0) {
+							newRow = 0;
+						}else if(newRow >= height) {
+							newRow = height - 1;
 						}
+						if(newCol < 0) {
+							newCol = 0;
+						}else if(newCol >= width) {
+							newCol = width - 1;
+						}
+						
+						int anIndex = newRow * width + newCol;
+						
+						redModify += redArray[anIndex] * stencil[count];
+						greenModify += greenArray[anIndex] * stencil[count];
+						blueModify += blueArray[anIndex] * stencil[count];
+						count++;
+						//System.out.println("row: " + row + " col: " + col + " count: " + count);
+
 					}
 				}
-				System.out.println("after");
 				modifiedRedArray[index] = redModify;
 				modifiedGreenArray[index] = greenModify;
 				modifiedBlueArray[index] = blueModify;
-				
-
-
 
 			}
 		}
-		System.out.println("here2");
 		for (int i = 0; i < sourceData.length; i++) {
-			
 
 			int newRed = modifiedRedArray[i];
 			int newGreen = modifiedGreenArray[i];
