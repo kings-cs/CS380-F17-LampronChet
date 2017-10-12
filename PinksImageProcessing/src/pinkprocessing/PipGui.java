de5a0bd648dfef56645431117cc2070e2be7aae4
cjlampron13 <cjlampron13@gmail.com>
2017-10-12 13:53:54 -0400

    checkstyle fix

--- PinksImageProcessing/src/pinkprocessing/PipGui.java
+++ PinksImageProcessing/src/pinkprocessing/PipGui.java
@@ -20,7 +20,7 @@ import java.io.FileNotFoundException;
 //import java.io.FileNotFoundException;
 //import java.io.FileReader;
 import java.io.IOException;
-import java.net.URL;
+//import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
