3cdf66ffa6ae29aaf4943808173d67ace075e2bc
chetlampron <chetlampron@ad42517>
2017-10-17 21:43:52 -0400

    All is working

--- PinksImageProcessing/src/algorithms/MosaicModifierParallel.java
+++ PinksImageProcessing/src/algorithms/MosaicModifierParallel.java
@@ -7,6 +7,7 @@ import java.awt.image.BufferedImage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Random;
 import java.util.Scanner;
 
 import javax.swing.JOptionPane;
@@ -26,10 +27,13 @@ import parallel.JoclInitializer;
  * @author Chet Lampron
  *
  */
-public class MosaicModifierParallel extends MosaicModifier {
+public class MosaicModifierParallel extends PixelModifier {
 
 	/** The device manager. */
 	private JoclInitializer deviceManager;
+
+	/** The number of tiles. */
+	private int tiles;
 
 	/**
 	 * Constructs a mosaic modifier for parallel computation.
@@ -40,8 +44,29 @@ public class MosaicModifierParallel extends MosaicModifier {
 	 *            The current device manager.
 	 */
 	public MosaicModifierParallel(int numOfTiles, JoclInitializer aDeviceManager) {
-		super(numOfTiles);
+		tiles = numOfTiles;
 		deviceManager = aDeviceManager;
+	}
+
+	/**
+	 * Generates the random pixels to be tile points in the mosaic.
+	 * 
+	 * @param numOfPoints
+	 *            The number of points to generate.
+	 * 
+	 * @param data
+	 *            The image data.
+	 * @return The tile points.
+	 */
+	public int[] getTilePoints(int numOfPoints, int[] data) {
+		int[] randomValues = new int[numOfPoints];
+		Random rand = new Random();
+
+		for (int i = 0; i < randomValues.length; i++) {
+			randomValues[i] = rand.nextInt(data.length - 1);
+		}
+
+		return randomValues;
 	}
 
 	@Override
@@ -51,18 +76,18 @@ public class MosaicModifierParallel extends MosaicModifier {
 		int[] sourceData = super.unwrapImage(image);
 
 		int[] resultData = new int[sourceData.length];
-		int[] tilePoints = getTilePoints(super.getTiles(), sourceData);
+		int[] tilePoints = getTilePoints(tiles, sourceData);
 		int[] dimensions = new int[3];
 
 		dimensions[0] = width;
 		dimensions[1] = height;
-		dimensions[2] = tilePoints.length;
+		dimensions[2] = tiles;
 
 		Pointer ptrSource = Pointer.to(sourceData);
 		Pointer ptrResult = Pointer.to(resultData);
 		Pointer ptrDimensions = Pointer.to(dimensions);
 		Pointer ptrTiles = Pointer.to(tilePoints);
-		
+
 		cl_mem memSource = CL.clCreateBuffer(deviceManager.getContext(), CL.CL_MEM_READ_ONLY | CL.CL_MEM_COPY_HOST_PTR,
 				Sizeof.cl_float * sourceData.length, ptrSource, null);
 		cl_mem memResult = CL.clCreateBuffer(deviceManager.getContext(), CL.CL_MEM_WRITE_ONLY | CL.CL_MEM_COPY_HOST_PTR,
@@ -70,7 +95,7 @@ public class MosaicModifierParallel extends MosaicModifier {
 		cl_mem memDimensions = CL.clCreateBuffer(deviceManager.getContext(),
 				CL.CL_MEM_READ_WRITE | CL.CL_MEM_COPY_HOST_PTR, Sizeof.cl_float * dimensions.length, ptrDimensions,
 				null);
-		cl_mem memTiles = CL.clCreateBuffer(deviceManager.getContext(), CL.CL_MEM_READ_ONLY,
+		cl_mem memTiles = CL.clCreateBuffer(deviceManager.getContext(), CL.CL_MEM_READ_WRITE | CL.CL_MEM_COPY_HOST_PTR,
 				Sizeof.cl_float * tilePoints.length, ptrTiles, null);
 
 		File kernelFile = new File("Kernels/MosaicKernel");
@@ -86,9 +111,7 @@ public class MosaicModifierParallel extends MosaicModifier {
 		CL.clBuildProgram(program, 0, null, null, null, null);
 
 		long[] globalWorkSize = new long[] { resultData.length };
-		// long[] localWorkSize = new long[] { super.getWorkSize(deviceManager,
-		// sourceData) };
-		long[] localWorkSize = new long[] { 1 };
+		long[] localWorkSize = new long[] { super.getWorkSize(deviceManager, sourceData) };
 		deviceManager.createQueue();
 
 		cl_kernel mosaicKernel = CL.clCreateKernel(program, "mosaic", null);
