ac723f1b7ef3548ed56478a766ce16c3a1520f8e
chetlampron <chetlampron@ad42518>
2017-11-14 13:54:55 -0500

    Implementing radix in sequence

---
+++ PinksImageProcessing/src/pinkprocessing/Radix.java
@@ -0,0 +1,46 @@
+package pinkprocessing;
+
+/**
+ * Class containing the methods for radix sort.
+ * 
+ * @author Chet Lampron
+ *
+ */
+public class Radix {
+	/** The work size. */
+	private int workSize;
+	
+	public Radix(int theWorkSize) {
+		workSize = theWorkSize;
+	}
+
+	public int[] isolateBit(int[] values, int bit) {
+		int[] returnBits = new int[values.length];
+		for(int i = 0; i < values.length; i++) {
+			returnBits[i] = (values[i] >> bit) & 1;
+		}
+		return returnBits;
+	}
+
+	public int[] flipBits(int[] values) {
+		int[] returnBits = new int[values.length];
+		for(int i = 0; i < values.length; i++) {
+			returnBits[i] = (values[i] ^ 1);
+		}
+		return returnBits;
+	}
+
+	public int[] scan(int[] values) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int[] calculateAdress(int[] values, int[] predicateValues, int[] normalScan, int[] predicateScan) {
+		int[] returnBits = new int[values.length];
+		for(int i = 0; i < values.length; i++) {
+			//TODO
+		}
+		return returnBits;
+	}
+
+}
--- PinksImageProcessing/src/testing/RadixTest.java
+++ PinksImageProcessing/src/testing/RadixTest.java
@@ -1,11 +1,14 @@
 package testing;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import org.junit.Test;
+
+import pinkprocessing.Radix;
+
 /**
  * Tester for radix sort.
+ * 
  * @author Chet Lampron
  *
  */
@@ -13,66 +16,86 @@ public class RadixTest {
 
 	@Test
 	public void testIsolate() {
-		int[] values = {10, 11, 2, 9, 0, 6, 1, 4, 7, 3, 8, 5};
-		
-		Radix radix = new Radix();
-		
-		int[] returnVal = radix.isolateBit();
-		
-		int[] expectedValues = {0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1};
-		
-		for(int i = 0; i < values.length; i++) {
-			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i], returnVal[i] == expectedValues[i]);
+		int[] values = { 10, 11, 2, 9, 0, 6, 1, 4, 7, 3, 8, 5 };
+
+		Radix radix = new Radix(1);
+
+		int[] returnVal = radix.isolateBit(values, 0);
+
+		int[] expectedValues = { 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1 };
+
+		for (int i = 0; i < values.length; i++) {
+			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i],
+					returnVal[i] == expectedValues[i]);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void testFlip() {
-		int[] values = {0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1};
-		
-		Radix radix = new Radix();
-		
-		int[] returnVal = radix.invertBit();
-		
-		int[] expectedValues = {1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0};
-		
-		for(int i = 0; i < values.length; i++) {
-			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i], returnVal[i] == expectedValues[i]);
+		int[] values = { 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1 };
+
+		Radix radix = new Radix(1);
+
+		int[] returnVal = radix.flipBits(values);
+
+		int[] expectedValues = { 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0 };
+
+		for (int i = 0; i < values.length; i++) {
+			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i],
+					returnVal[i] == expectedValues[i]);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void testScanNormal() {
-		int[] values = {0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1};
-		
-		Radix radix = new Radix();
-		
-		int[] returnVal = radix.invertBit();
-		
-		int[] expectedValues = {0, 0, 1, 1, 2, 3, 3, 4, 4, 4, 5, 5};
-		
-		for(int i = 0; i < values.length; i++) {
-			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i], returnVal[i] == expectedValues[i]);
+		int[] values = { 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1 };
+
+		Radix radix = new Radix(1);
+
+		int[] returnVal = radix.scan(values);
+
+		int[] expectedValues = { 0, 0, 1, 1, 2, 3, 3, 4, 4, 4, 5, 5 };
+
+		for (int i = 0; i < values.length; i++) {
+			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i],
+					returnVal[i] == expectedValues[i]);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void testScanPredicate() {
-		int[] values = {1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0};
-		
-		Radix radix = new Radix();
-		
-		int[] returnVal = radix.invertBit();
-		
-		int[] expectedValues = {0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 4, 5};
-		
-		for(int i = 0; i < values.length; i++) {
-			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i], returnVal[i] == expectedValues[i]);
+		int[] values = { 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0 };
+
+		Radix radix = new Radix(1);
+
+		int[] returnVal = radix.scan(values);
+
+		int[] expectedValues = { 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 4, 5 };
+
+		for (int i = 0; i < values.length; i++) {
+			assertTrue("Should return " + expectedValues[i] + " but was " + returnVal[i],
+					returnVal[i] == expectedValues[i]);
 		}
-		
+
+	}
+
+	@Test
+	public void testAdress() {
+		int[] values = { 0, 1, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1 };
+		int[] predicateValues = { 1, 0, 1, 0, 1, 1, 0, 1, 0, 0, 1, 0 };
+		int[] normalScan = { 0, 0, 1, 1, 2, 3, 3, 4, 4, 4, 5, 5 };
+		int[] predicateScan = { 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 4, 5 };
+		int[] expectedValues = { 10, 2, 0, 6, 4, 8, 11, 9, 1, 7, 3, 5 };
+
+		Radix radix = new Radix(1);
+
+		int[] result = radix.calculateAdress(values, predicateValues, normalScan, predicateScan);
+		for (int i = 0; i < values.length; i++) {
+			assertTrue("Should return " + expectedValues[i] + " but was " + result[i], result[i] == expectedValues[i]);
+		}
 	}
 
 }
